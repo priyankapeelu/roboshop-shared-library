7e4ce1470eb9ab3eace3023ee115912141d5966f
priyankapeelu <priyankapeelu@icloud.com>
2022-04-11 18:51:57 +0530

    jenkinsfile added

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -7,7 +7,7 @@ def lintChecks() {
 }
 
 def sonarCheck() {
-    ssh '''
+    sh '''
       sonar-scanner -Dsonar.host.url=http://172.31.13.177:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
      '''
 }
