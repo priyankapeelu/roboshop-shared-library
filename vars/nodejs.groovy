35d554b61c93a6dc8907e04e80a275db07124ea4
priyankapeelu <priyankapeelu@icloud.com>
2022-04-11 17:14:19 +0530

    jenkinsfile added

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -2,11 +2,11 @@ def lintChecks() {
     sh '''
     # we commented this because devs gonna check the failures
     #~/node_modules/jslint/bin/jslint.js server.js
-    echo Lint Check
+    echo Lint Check for ${COMPONENT}
   '''
 }
 
-def call() {
+def call(COMPONENT) {
 
     pipeline {
         agent any
