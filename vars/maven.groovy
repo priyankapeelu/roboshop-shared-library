52f0337b6b9fea9cb1cffe9d8b10032ce58933c5
priyankapeelu <priyankapeelu@icloud.com>
2022-04-13 18:47:01 +0530

    jenkinsfile added

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,65 +1,15 @@
-def lintChecks() {
-    sh '''
-    # We commented this because devs gonna check the failures.
-    #~/node_modules/jslint/bin/jslint.js server.js
-    #mvn checkstyle:check
-    echo Lint Check for ${COMPONENT}
-  '''
-}
-
 def call() {
-    pipeline {
-        agent any
+    node {
+        git branch: 'main', url: "https://github.com/raghudevopsb63/${COMPONENT}"
+        env.APP_TYPE = "maven"
+        common.lintChecks()
+        sh 'mvn clean compile'
+        env.ARGS="-Dsonar.java.binaries=target/"
+        common.sonarCheck()
+        common.testCases()
 
-        environment {
-            SONAR = credentials('SONAR')
+        if (env.TAG_NAME != null) {
+            common.artifacts()
         }
-
-        stages {
-
-            // For Each Commit
-            stage('Lint Checks') {
-                steps {
-                    script {
-                        lintChecks()
-                    }
-                }
-            }
-
-            stage('SonarCheck') {
-                steps {
-                    script {
-                        sh 'mvn clean compile'
-                        env.ARGS="-Dsonar.java.binaries=target/"
-                        common.sonarCheck()
-                    }
-                }
-            }
-            stage('Test Cases') {
-                parallel {
-
-                    stage('unit tests') {
-                        steps {
-                            sh 'echo unit Tests'
-                        }
-                    }
-                    stage('Integration Test') {
-                        steps {
-                            sh 'echo Integration Tests'
-                        }
-                    }
-                    stage('Functional tests') {
-                        steps {
-                            sh 'echo Functionnal Tests'
-                        }
-                    }
-
-                }
-            }
-
-        } // End of Stages
-
     }
-
-
 }
