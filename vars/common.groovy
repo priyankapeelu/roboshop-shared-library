4494a54781d6aee763cda158e0a47fa1a21641cb
priyankapeelu <priyankapeelu@icloud.com>
2022-04-13 18:16:29 +0530

    jenkinsfile added

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,8 +1,111 @@
 def sonarCheck() {
+    stage('Sonar Code Analysis') {
+        sh '''
+      #sonar-scanner -Dsonar.host.url=http://172.31.4.85:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}
+      #sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.4.85 ${COMPONENT}
+      echo Sonar Checks for ${COMPONENT}
+    '''
+    }
+}
 
-    sh '''
-    #sonar-scanner -Dsonar.host.url=http://172.31.13.177:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}
-    #sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.13.177 ${COMPONENT}
-    echo sonar check for ${COMPONENT}
-  '''
+
+def lintChecks() {
+    stage('Lint Checks') {
+        if (env.APP_TYPE == "nodejs") {
+            sh '''
+        # We commented this because devs gonna check the failures.
+        #~/node_modules/jslint/bin/jslint.js server.js
+        echo Link Check for ${COMPONENT}
+      '''
+        }
+        else if (env.APP_TYPE == "maven") {
+            sh '''
+      # We commented this because devs gonna check the failures.
+      #~/node_modules/jslint/bin/jslint.js server.js
+      #mvn checkstyle:check
+      echo Lint Check for ${COMPONENT}
+    '''
+        }
+        else if (env.APP_TYPE == "python" ) {
+            sh '''
+        # We commented this because devs gonna check the failures.
+        #~/node_modules/jslint/bin/jslint.js server.js
+        #pylint *.py
+        echo Lint Check for ${COMPONENT}
+      '''
+        }
+        else if (env.APP_TYPE == "golang" ){
+            sh '''
+        # We commented this because devs gonna check the failures.
+        #~/node_modules/jslint/bin/jslint.js server.js
+        echo Link Check for ${COMPONENT}
+      '''
+        }
+    }
+
+}
+
+def testCases() {
+
+    stage('Test Cases') {
+        def stages = [:]
+
+        stages["Unit Tests"] = {
+            sh 'echo Unit Tests'
+        }
+        stages["Integration Tests"] = {
+            sh 'echo Integration Tests'
+        }
+
+        stages["Functional Tests"] = {
+            sh 'echo Functional Tests'
+        }
+
+        parallel(stages)
+    }
+
+}
+
+
+def artifacts() {
+
+    stage('Check The Release') {
+        env.UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -L -s http://172.31.0.90:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true')
+        print UPLOAD_STATUS
+    }
+
+    if(env.UPLOAD_STATUS == "") {
+
+        stage('Prepare Artifacts') {
+            if (env.APP_TYPE == "nodejs") {
+                sh '''
+          ls -l
+          npm install 
+          ls -l
+          zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js 
+        '''
+            } else if (env.APP_TYPE == "maven") {
+                sh '''
+         echo 
+        '''
+            } else if (env.APP_TYPE == "python") {
+                sh '''
+          echo 
+        '''
+            } else if (env.APP_TYPE == "golang") {
+                sh '''
+          echo 
+        '''
+            }
+        }
+
+        stage('Upload Artifacts') {
+            withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
+                sh '''
+        curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://172.31.0.90:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+      '''
+            }
+        }
+
+    }
 }
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,103 +1,14 @@
-def lintChecks() {
-    sh '''
-    # we commented this because devs gonna check the failures
-    #~/node_modules/jslint/bin/jslint.js server.js
-    echo Lint Check for ${COMPONENT}
-  '''
-}
-
 def call() {
-    pipeline {
-        agent any
+    node {
+        git branch: 'main', url: "https://github.com/raghudevopsb63/${COMPONENT}"
+        env.APP_TYPE = "nodejs"
+        common.lintChecks()
+        env.ARGS="-Dsonar.sources=."
+        common.sonarCheck()
+        common.testCases()
 
-        environment {
-            SONAR = credentials('SONAR')
-            NEXUS = credentials('NEXUS')
+        if (env.TAG_NAME != null) {
+            common.artifacts()
         }
-
-        stages {
-
-            //for each commit
-            stage('lint checks') {
-                steps {
-                    script {
-                        lintChecks()
-                    }
-                }
-            }
-
-            stage('Sonar Check') {
-                steps {
-                    script {
-                        env.ARGS = "-Dsonar.sources=."
-                        common.sonarCheck()
-                    }
-                }
-            }
-            stage('Test Cases') {
-                parallel {
-
-                    stage('unit tests') {
-                        steps {
-                            sh 'echo unit Tests'
-                        }
-                    }
-                    stage('Integration Test') {
-                        steps {
-                            sh 'echo Integration Tests'
-                        }
-                    }
-                    stage('Functional tests') {
-                        steps {
-                            sh 'echo Functionnal Tests'
-                        }
-                    }
-
-                }
-
-            }
-
-            stage('check the release') {
-                when {
-                    expression { env.TAG_NAME != null }
-                }
-                steps {
-                    script {
-                        def UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://172.31.5.42:8081/service/rest/repository/browse/${COMPONENT} | grep \${COMPONENT}-\${TAG_NAME}.zip || true")
-                        print UPLOAD_STATUS
-                    }
-
-                }
-            }
-                stage('prepare Artifacts') {
-                    when {
-                        expression { env.TAG_NAME != null }
-                        expression { env.UPLOAD_STATUS == "" }
-                    }
-                    steps {
-                        sh '''
-                          npm install 
-                          zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
-                        '''
-
-
-                    }
-                }
-
-                stage('Upload Artifacts') {
-                    when {
-                        expression { env.TAG_NAME != null }
-                        expression { env.UPLOAD_STATUS == "" }
-                    }
-                    steps {
-                        sh '''
-                          curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://172.31.5.42:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
-                          '''
-                    }
-
-                }
-
-            }
-        }
-
     }
+}
