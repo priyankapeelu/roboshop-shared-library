a80451d245464f8c36fa978e1baea3a4a2f0adf7
priyankapeelu <priyankapeelu@icloud.com>
2022-04-13 18:23:08 +0530

    jenkinsfile added

--- vars/common.groovy
+++ vars/common.groovy
@@ -102,7 +102,7 @@ def artifacts() {
         stage('Upload Artifacts') {
             withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
                 sh '''
-        curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://172.31.5.428081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+        curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://172.31.5.42:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
       '''
             }
         }
