a12ab43bf5b4009d0dad658ac470e53023a56da9
priyankapeelu <priyankapeelu@icloud.com>
2022-04-15 15:25:53 +0530

    test

--- vars/common.groovy
+++ vars/common.groovy
@@ -112,6 +112,7 @@ def artifacts() {
             else if (env.APP_TYPE == "nginx" ){
                 sh '''
           cd static
+          zip -r ../${COMPONENT}-${TAG_NAME}.zip *
           '''
 
             }
