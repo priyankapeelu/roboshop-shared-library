fce17b185e1ed017d3248e84092e7bb57819721b
priyankapeelu <priyankapeelu@icloud.com>
2022-04-22 17:07:23 +0530

    test

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -7,22 +7,27 @@ def call() {
     ])
 
     node {
+        ansiColor('xterm') {
             sh 'rm -rf *'
-            git branch: 'main', url: "https://github.com/priyankapeelu/${REPONAME}"
+            git branch: 'main', url: "https://github.com/raghudevopsb63/${REPONAME}"
 
             stage('Terrafile INIT') {
                 sh 'terrafile -f env-${ENV}/Terrafile'
-
-
-                stage('Terraform INIT') {
-                    sh 'terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars'
-                }
-
-                stage('Terraform Plan') {
-                    sh 'terraform plan -var-file=env-${ENV}/${ENV}.tfvars'
-                }
             }
+
+            stage('Terraform INIT') {
+                sh 'terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars'
+            }
+
+            stage('Terraform Plan') {
+                sh 'terraform plan -var-file=env-${ENV}/${ENV}.tfvars'
+            }
+
+            stage('Terraform Apply') {
+                sh 'terraform apply -var-file=env-${ENV}/${ENV}.tfvars -auto-approve'
+            }
+
+        }
     }
 }
-
 
