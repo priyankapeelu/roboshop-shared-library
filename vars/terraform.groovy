1685d0ff26beffb2104ada051cde9eca00adae94
priyankapeelu <priyankapeelu@icloud.com>
2022-05-19 18:11:41 +0530

    terraform variablescd tt

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,33 +1,51 @@
 def call() {
 
+    if (!env.TERRAFORM_DIR) {
+        env.TERRAFORM_DIR = "./"
+    }
+
     properties([
             parameters([
-                    choice(choices: 'dev\nprod', description: "Choose Environement", name: "ENV"),
+                    choice(choices: 'dev\nprod', description: "Choose Environment", name: "ENV"),
+                    string(choices: 'APP_VERSION', description: "APP VERSION", name: "APP_VERSION"),
             ]),
     ])
 
     node {
         ansiColor('xterm') {
             sh 'rm -rf *'
-            git branch: 'main', url: "https://github.com/raghudevopsb63/${REPONAME}"
+            git branch: 'main', url: "https://github.com/priyankapeelu/${REPONAME}"
 
             stage('Terrafile INIT') {
-                sh 'terrafile -f env-${ENV}/Terrafile'
+                sh '''
+          cd ${TERRAFORM_DIR}
+          terrafile -f env-${ENV}/Terrafile
+        '''
             }
 
             stage('Terraform INIT') {
-                sh 'terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars'
+                sh '''
+          cd ${TERRAFORM_DIR}
+          terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars
+        '''
             }
 
             stage('Terraform Plan') {
-                sh 'terraform plan -var-file=env-${ENV}/${ENV}.tfvars'
+                sh '''
+          cd ${TERRAFORM_DIR}
+          export TF_VAR_APP_VERSION=${APP_VERSION}
+          terraform plan -var-file=env-${ENV}/${ENV}.tfvars 
+        '''
             }
 
             stage('Terraform Apply') {
-                sh 'terraform apply -var-file=env-${ENV}/${ENV}.tfvars -auto-approve'
+                sh '''
+          cd ${TERRAFORM_DIR}
+          export TF_VAR_APP_VERSION=${APP_VERSION}
+          terraform apply -var-file=env-${ENV}/${ENV}.tfvars -auto-approve
+        '''
             }
 
         }
     }
 }
-
