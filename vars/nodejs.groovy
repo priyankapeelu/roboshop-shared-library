bf79f0c6480092b2cba34e1f530423b26971d2ce
priyankapeelu <priyankapeelu@icloud.com>
2022-06-02 21:46:07 +0530

    terraform variablescd tt

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -79,7 +79,7 @@ def call() {
                 }
                 steps {
                     sh '''
-            curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://172.31.11.69:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+            curl -f -v -u admin:admin --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://172.31.11.69:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
           '''
                 }
             }
