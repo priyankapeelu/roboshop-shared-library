bb142bda3c47e14635a3b866e0a7b40f2d529856
priyankapeelu <priyankapeelu@icloud.com>
2022-07-05 16:13:36 +0530

    terraform variablescd tt

--- vars/common.groovy
+++ vars/common.groovy
@@ -123,7 +123,7 @@ def artifacts() {
     }
 
     stage('Upload Artifacts') {
-        #withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
+        withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
             sh '''
         #curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://172.31.0.155:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
         curl -v -u admin:admin --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://ip-172-31-19-185.ec2.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
